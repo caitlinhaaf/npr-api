8efe245c612723341036c47614f338151e2ba91f
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2016-04-18 20:02:09 -0400

    Fixed the 'current search' call with generated date variable; fixed the audio.pause() issue but still having issue with changing the state of the button to off when paused.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 var app = angular.module('myApp', []);
 
-// MDIzODc0NDUxMDE0NjA5NDc4NzYzY2U5ZQ000
-// ztWjHgv1SlImUnquVnT3MSZgBOFUErO8Fd2qOoXs
+// get/generate current date object
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+if(dd<10) dd='0'+dd;
+if(mm<10) mm='0'+mm;
 
+var todaysDate = yyyy+"-"+mm+"-"+"dd";
+// console.log(dd,mm,yyyy);
+
+// api calls and key
 var apiKey = 'MDIzODc0NDUxMDE0NjA5NDc4NzYzY2U5ZQ000',
-    //queries most recent shows - runs when app play contorller loads
-    // recentNprUrl = 'http://api.npr.org/query?id=61&fields=all&output=JSON';
-    recentNprUrl = 'http://api.npr.org/query?id=61&fields=title,teaser,audio,image,show,storyDate,none&output=JSON';
+    //queries most 10 recent shows - runs when app play contorller loads
+    recentNprUrl =
+    'http://api.npr.org/query?requiredAssets=audio&startDate='+todaysDate+'&dateType=story&fields=title,teaser,audio,image,show,storyDate&sort=dat&output=JSON'
+
+    //
     searchNprUrl =
-    'http://api.npr.org/list?id=3004&output=JSON'
-    // nprUrl = 'http://api.npr.org/query?id=448706447'
+    'http://api.npr.org/list?id=3002&numResults=2&output=JSON';
+
 
 //PLAY LIST/CONTROLLER
 app.controller('PlayerController', function($scope, $http) {
@@ -27,24 +38,20 @@ app.controller('PlayerController', function($scope, $http) {
     $scope.selectedIndex = index;
 
     //change button class
-    if ($scope.class === "buttonOff")
-        $scope.class = "buttonOn";
-    else $scope.class = "buttonOff";
+    if ($scope.class === "buttonOff") $scope.class = "buttonOn";
+    else {$scope.class = "buttonOff"}
 
     if ($scope.playing){
       audio.pause();
+      $scope.playing = false;
       // return;
-    }
-    var url = program.audio[0].format.mp4.$text;
-    // testing...
-    // var url = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
-    if(program.audio[0].format != 'undefined'){
+    }else{
+      var url = program.audio[0].format.mp4.$text;
+      // testing...
+      // var url = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
       audio.src = url;
       audio.play();
       $scope.playing = true;//store playing state...
-
-    }else{
-      console.log("not available");
     }
   }
 
